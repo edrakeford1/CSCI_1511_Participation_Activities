7076175629ab001889f34f94ab253942aa775bab
edrakeford1 <edrakeford@student.cscc.edu>
2026-02-03 13:10:08 -0500

    Add files via upload

---
+++ week_2_participation.py
@@ -0,0 +1,35 @@
+
+"""
+Program: 3-8 Seeing The World, Week 2 Participation Activity
+
+Name: Elijah Drakeford
+
+Date: Feb 1, 2026
+"""
+
+# creating list
+see_world = ['south korea', 'hawaii', 'california', 'nigeria', 'england']
+
+print(see_world)
+
+# printing sorted list
+print(sorted(see_world))
+
+# showing list is still in original order
+print(see_world)
+
+# reversing the order of list
+see_world.reverse()
+print(see_world)
+
+# reversing list back to it's original order
+see_world.reverse()
+print(see_world)
+
+# sorting list in alphabetical order
+see_world.sort()
+print(see_world)
+
+# sorting list in reverse alphabetical order
+see_world.sort(reverse=True)
+print(see_world)

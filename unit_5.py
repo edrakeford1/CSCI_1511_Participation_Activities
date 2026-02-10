b013c8cd238772e6a16a6d5329cf75c09401f084
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-02-10 14:45:54 -0500

    Alien colors #3 if color is green

---
+++ unit_5.py
@@ -0,0 +1,15 @@
+"""
+Program: 5-5 Alien Colors #3, Unit 5 Participation Activity
+
+Name: Elijah Drakeford
+
+Date: Feb 10, 2026
+"""
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("The player has earned five points")
+elif alien_color == 'yellow':
+    print("The player has earned ten points")
+elif alien_color == 'red':
+    print("The player has earned fifteen points")

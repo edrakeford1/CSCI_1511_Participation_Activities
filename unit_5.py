53be3a60db77d8871d53bb6b402035c9ad8b2cc8
Elijah Drakeford <elijahdrakeford12@gmail.com>
2026-02-10 14:28:35 -0500

    Alien colors #1 input passes

---
+++ unit_5.py
@@ -0,0 +1,11 @@
+"""
+Program: 5-3 Alien Colors #1, Unit 5 Participation Activity
+
+Name: Elijah Drakeford
+
+Date: Feb 10, 2026
+"""
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("The player has earned five points")
